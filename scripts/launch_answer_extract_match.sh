f18f87c5d805ed52d9aec239b958d6de451b6d2c
wanqian5 <wanqian5@tal.com>
2024-03-19 16:38:54 +0800

    colocate answer extract and match

--- scripts/launch_answer_extract_match.sh
+++ scripts/launch_answer_extract_match.sh
@@ -2,14 +2,6 @@
 
 set -x
 
-run_mode=extract
-
-if [[ "$run_mode" = "extract" ]]; then
-  MODEL_PATH=/mnt/pfs/jinfeng_team/SFT/wanqian/yq9/models/math-policy/qwen_14b_answer_extract_gpt4_clean_english_4096_0116
-else
-  MODEL_PATH=/mnt/pfs/jinfeng_team/SFT/wanqian/yq9/models/math-policy/qwen_14b_timu1_finegrained_label_instruct_0109
-fi
-
 openai_compatible=0
 
 if [[ $openai_compatible -eq 0 ]]; then
@@ -20,9 +12,18 @@ else
   model_name_arg=""
 fi
 
+extract_model=/mnt/pfs/jinfeng_team/SFT/wanqian/yq9/models/math-policy/qwen_14b_answer_extract_gpt4_clean_english_4096_0116
 for i in `seq 0 7`;
 do
-  CUDA_VISIBLE_DEVICES=$i nohup python -m vllm.entrypoints${openai_infix}.api_server --port 100$i --model $MODEL_PATH \
-    --trust-remote-code --disable-log-stats --enforce-eager -tp 1 $model_name_arg &>/dev/null &
+  CUDA_VISIBLE_DEVICES=$i nohup python -m vllm.entrypoints${openai_infix}.api_server --port 100$i --model extract_model \
+    --trust-remote-code --disable-log-stats --enforce-eager -tp 1 $model_name_arg --gpu-memory-utilization 0.4 &>/dev/null &
 done
+
+match_model=/mnt/pfs/jinfeng_team/SFT/wanqian/yq9/models/math-policy/qwen_14b_timu1_finegrained_label_instruct_0109
+for i in `seq 0 7`;
+do
+  CUDA_VISIBLE_DEVICES=$i nohup python -m vllm.entrypoints${openai_infix}.api_server --port 200$i --model extract_model \
+    --trust-remote-code --disable-log-stats --enforce-eager -tp 1 $model_name_arg --gpu-memory-utilization 0.4 &>/dev/null &
+done
+
 wait
